263ba0c94ceb8b6268d9c1765f990a59135cde33
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-03-20 06:06:23 +0500

    added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,52 +13,109 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: 
-Container(
-      child: Column(
-        children: [
-          Container(
-            height: 220,
-            padding: EdgeInsets.only(left: 20.0, top: 50.0),
-            decoration: BoxDecoration(
-              color: Color(
-                0xff2a2b31,
-              ),
-                  borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(30.0),
-                  bottomRight: Radius.circular(30.0),
-                ),
-            ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
               children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(60.0),
-                  child: Image.asset(
-                    "/images/boy.jpg",
-                    height: 50,
-                    width: 50,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                SizedBox(width: 20.0),
-                Padding(
-                  padding: EdgeInsets.only(top: 10.0),
-                  child: Text(
-                    "Flutter Developer",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
+                Container(
+                  height: 220,
+                  padding: EdgeInsets.only(left: 20.0, top: 50.0),
+                  decoration: BoxDecoration(
+                    color: Color(
+                      0xff2a2b31,
+                    ),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(30.0),
+                      bottomRight: Radius.circular(30.0),
                     ),
                   ),
-                )
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(60.0),
+                        child: Image.asset(
+                          "/images/boy.jpg",
+                          height: 50,
+                          width: 50,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(width: 20.0),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                        child: Text(
+                          "Flutter Developer",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                    margin:
+                        EdgeInsets.only(top: 120.0, right: 20.0, left: 20.0),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(30)),
+                    child: Row(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              bottomLeft: Radius.circular(20)),
+                          child: Image.asset(
+                            "images/boy.jpg",
+                            width: 100,
+                            height: 100,
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Play and win",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 30.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              "Play quiz by guessing\n a number",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.w500),
+                            )
+                          ],
+                        )
+                      ],
+                    ))
               ],
             ),
-          )
-        ],
+            SizedBox(
+              height: 30.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Text(
+                "Top quiz Categories",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 23.0,
+                    fontWeight: FontWeight.w500),
+              ),
+            )
+          ],
+        ),
       ),
-    ),
     );
   }
 }
